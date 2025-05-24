8fab04a839436d47897fd3bf962fa9df455185ec
Aytac G <aytacgenc1@gmail.com>
2025-05-24 23:46:53 +0300

    test cases improved

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://practicesoftwaretesting.com/')
+describe('testing login page', () => {
+  it('should visit site and register', () => {
+    cy.visit('https://practicesoftwaretesting.com/');
+    cy.get('[data-test="nav-sign-in"]').click();
+    cy.get('[data-test="email"]').type('aytac@aytac.com');
+    cy.get('[data-test="password"]').type('$implePass1');
+    cy.get('[data-test="login-submit"]').click();
+
+    cy.get('[data-test="nav-menu"]').contains('Aytac Genc');
   })
 })
